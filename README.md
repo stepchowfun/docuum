dba37923a62535fce78934131675aa9e5d554dc6
Stephan Boyer <stephan@stephanboyer.com>
2020-01-04 01:55:06 -0800

    Set up a 'Hello, World!' Rust project

---
+++ README.md
@@ -0,0 +1,50 @@
+# Docuum
+
+[![Build Status](https://travis-ci.org/docuumlang/docuum.svg?branch=master)](https://travis-ci.org/docuumlang/docuum)
+
+*Tagref* performs LRU cache eviction for Docker images.
+
+## Installation
+
+### Easy installation
+
+If you are running macOS or a GNU-based Linux on an x86-64 CPU, you can install Docuum with this command:
+
+```sh
+curl https://raw.githubusercontent.com/docuumlang/docuum/master/install.sh -LSfs | sh
+```
+
+The same command can be used again to update Docuum to the latest version.
+
+**NOTE:** Piping `curl` to `sh` is dangerous since the server might be compromised. If you're concerned about this, you can download and inspect the installation script or choose one of the other installation methods.
+
+#### Customizing the installation
+
+The installation script supports the following environment variables:
+
+- `VERSION=x.y.z` (defaults to the latest version)
+- `PREFIX=/path/to/install` (defaults to `/usr/local/bin`)
+
+For example, the following will install Docuum into the working directory:
+
+```sh
+curl https://raw.githubusercontent.com/docuumlang/docuum/master/install.sh -LSfs | PREFIX=. sh
+```
+
+### Manual installation
+
+The [releases page](https://github.com/docuumlang/docuum/releases) has precompiled binaries for macOS or Linux systems running on an x86-64 CPU. You can download one of them and place it in a directory listed in your [`PATH`](https://en.wikipedia.org/wiki/PATH_\(variable\)).
+
+### Installation with Cargo
+
+If you have [Cargo](https://doc.rust-lang.org/cargo/), you can install Docuum as follows:
+
+```sh
+cargo install docuum
+```
+
+You can run that command with `--force` to update an existing installation.
+
+## Requirements
+
+- Docuum requires [Docker Engine](https://www.docker.com/products/container-runtime) 17.03.0 or later.
---
+++ install.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env sh
+
+# Usage examples:
+#   ./install.sh
+#   VERSION=x.y.z ./install.sh
+#   PREFIX=/usr/local/bin ./install.sh
+
+# We wrap everything in parentheses for two reasons:
+# 1. To prevent the shell from executing only a prefix of the script if the download is interrupted
+# 2. To ensure that any working directory changes with `cd` are local to this script and don't
+#    affect the calling user's shell
+(
+  # Where the binary will be installed
+  DESTINATION="${PREFIX:-/usr/local/bin}/docuum"
+
+  # Which version to download
+  RELEASE="v${VERSION:-0.0.0}"
+
+  # Determine which binary to download.
+  FILENAME=''
+  if uname -a | grep -qi 'x86_64.*GNU/Linux'; then
+    echo 'x86_64 GNU/Linux detected.'
+    FILENAME=docuum-x86_64-unknown-linux-gnu
+  fi
+  if uname -a | grep -qi 'Darwin.*x86_64'; then
+    echo 'macOS detected.'
+    FILENAME=docuum-x86_64-apple-darwin
+  fi
+
+  # Find a temporary location for the binary.
+  TEMPDIR=$(mktemp -d /tmp/docuum.XXXXXXXX)
+
+  # This is a helper function to clean up and fail.
+  fail() {
+    echo "$1" >&2
+    cd "$TEMPDIR/.." || exit 1
+    rm -rf "$TEMPDIR"
+    exit 1
+  }
+
+  # Enter the temporary directory.
+  cd "$TEMPDIR" || fail "Unable to access the temporary directory $TEMPDIR."
+
+  # Fail if there is no pre-built binary for this platform.
+  if [ -z "$FILENAME" ]; then
+    fail 'Unfortunately, there is no pre-built binary for this platform.'
+  fi
+
+  # Download the binary.
+  if ! curl "https://github.com/docuumlang/docuum/releases/download/$RELEASE/$FILENAME" \
+      -o "$FILENAME" -LSf; then
+    fail 'There was an error downloading the binary.'
+  fi
+
+  # Make it executable.
+  if ! chmod a+rx "$FILENAME"; then
+    fail 'There was an error setting the permissions for the binary.'
+  fi
+
+  # Install it at the requested destination.
+  # shellcheck disable=SC2024
+  mv "$FILENAME" "$DESTINATION" 2> /dev/null ||
+    sudo mv "$FILENAME" "$DESTINATION" < /dev/tty ||
+    fail "Unable to install the binary at $DESTINATION."
+
+  # Remove the temporary directory.
+  cd ..
+  rm -rf "$TEMPDIR"
+
+  # Let the user know it worked.
+  echo "$("$DESTINATION" --version) is now installed."
+)
