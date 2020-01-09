02aeaa2233c23046dfc0d0ccb2d04b4f0ced4272
Stephan Boyer <stephan@stephanboyer.com>
2020-01-08 20:07:58 -0800

    Install v0.6.0 by default

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@
   DESTINATION="${PREFIX:-/usr/local/bin}/docuum"
 
   # Which version to download
-  RELEASE="v${VERSION:-0.5.0}"
+  RELEASE="v${VERSION:-0.6.0}"
 
   # Determine which binary to download.
   FILENAME=''
