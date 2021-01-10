b6ad7246af97b755ed593d25455c9ed29fe4bece
Markus Lippert <lippertmarkus@gmx.de>
2021-01-10 15:02:36 +0100

    add integration test for Windows

--- integration_test.sh
+++ integration_test.sh
@@ -11,7 +11,14 @@ done
 
 # Start Docuum in the background.
 echo 'Starting Docuum…'
-/artifacts/docuum-x86_64-unknown-linux-musl --threshold 13MB &
+if $WINDOWS; then
+  # Windows integration tests doesn't run with dind so we need to prune all images on the CI worker
+  echo 'Pruning images on Windows CI worker…'
+  docker image prune -a -f
+  target/release/docuum.exe --threshold 725MB &
+else
+  /artifacts/docuum-x86_64-unknown-linux-musl --threshold 13MB &
+fi
 DOCUUM_PID="$!"
 
 # This function waits for Docuum to start sleeping by checking the process state. The process could
@@ -28,30 +35,44 @@ wait_for_docuum() {
 
 wait_for_docuum
 
-# This image uses ~5.5 MB.
+if $WINDOWS; then
+  # This image uses ~347 MB.
+  IMAGE1="mcr.microsoft.com/windows/nanoserver@sha256:4fbb09b6f685754391b86d86399d3a52f95ed0e6e5174947e205dd86df90cf3a"
+  # This image uses ~252 MB.
+  IMAGE2="mcr.microsoft.com/windows/nanoserver@sha256:5e8cbdb57a33156c9f37bec01b6add720532d1432ce4ade821920c4f7b8c6409"
+  # This image also uses ~252 MB. Now we should be above the 725 MB threshold.
+  IMAGE3="mcr.microsoft.com/windows/nanoserver@sha256:5f7004ad6559594da4e44e661f4feee58f291ed01ef71717a2041c40d45655d8"
+else
+  # This image uses ~5.5 MB.
+  IMAGE1="alpine@sha256:4716d67546215299bf023fd80cc9d7e67f4bdc006a360727fd0b0b44512c45db"
+  # This image also uses ~5.5 MB.
+  IMAGE2="alpine@sha256:fef20cf0221c5c0eaae2d8f59081b07cd351a94ac83cdc74109b17ec90ce0a82"
+  # This image also uses ~5.5 MB. Now we should be above the 13 MB threshold.
+  IMAGE3="alpine@sha256:6b987122c635cd4bf46e52d85bca765732c7a224866501742c549ccc852f8c53"
+fi
+
+
 echo 'Using an image…'
-docker run alpine@sha256:4716d67546215299bf023fd80cc9d7e67f4bdc006a360727fd0b0b44512c45db true
+docker run $IMAGE1
 
-# This image also uses ~5.5 MB.
 echo 'Using another image…'
-docker run alpine@sha256:fef20cf0221c5c0eaae2d8f59081b07cd351a94ac83cdc74109b17ec90ce0a82 true
+docker run $IMAGE2
 
-# This image also uses ~5.5 MB. Now we should be above the 13 MB threshold.
 echo 'Using a third image…'
-docker run alpine@sha256:6b987122c635cd4bf46e52d85bca765732c7a224866501742c549ccc852f8c53 true
+docker run $IMAGE3
 
 wait_for_docuum
 
 # Assert the two most recently used images are still present.
 echo 'Checking that the last two images are still present…'
-docker inspect alpine@sha256:6b987122c635cd4bf46e52d85bca765732c7a224866501742c549ccc852f8c53 > \
+docker inspect $IMAGE3 > \
   /dev/null 2>&1
-docker inspect alpine@sha256:fef20cf0221c5c0eaae2d8f59081b07cd351a94ac83cdc74109b17ec90ce0a82 > \
+docker inspect $IMAGE2 > \
   /dev/null 2>&1
 
 # Assert the first image was deleted.
 echo 'Checking that the first image was deleted…'
-if docker inspect alpine@sha256:4716d67546215299bf023fd80cc9d7e67f4bdc006a360727fd0b0b44512c45db \
+if docker inspect $IMAGE1 \
   > /dev/null 2>&1
 then
   echo "The image wasn't deleted."
