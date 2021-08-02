15e16e8d130f537f5894369cccf9afb99642aee2
Stephan Boyer <stephan@stephanboyer.com>
2021-08-02 14:18:29 -0700

    Use a smarter strategy for "last used" timestamp default values and release v0.20.2

--- integration_test.sh
+++ integration_test.sh
@@ -11,18 +11,20 @@ done
 
 # Start Docuum in the background.
 echo 'Starting Docuum…'
-/docuum-x86_64-unknown-linux-musl --threshold '20 MB' --keep 'alpine:keep' &
+LOG_LEVEL=debug /docuum-x86_64-unknown-linux-musl --threshold '14 MB' --keep 'alpine:keep' &
 DOCUUM_PID="$!"
 
 # This function waits for Docuum to start sleeping by checking the process state. The process could
 # be sleeping for many reasons, so this function may return prematurely. In that case, the
-# integration test will fail flakily. To make that outcome less likely, we sleep for 10 seconds
-# before checking the process state.
+# integration test may fail flakily. To make that outcome less likely, we repeat this procedure
+# several times at a 1 second interval.
 wait_for_docuum() {
   echo 'Waiting for Docuum to sleep…'
-  sleep 10
-  while [[ "$(awk '{ print $3 }' /proc/$DOCUUM_PID/stat)" != 'S' ]]; do
+  for _ in {0..4}; do
     sleep 1
+    while [[ "$(awk '{ print $3 }' /proc/$DOCUUM_PID/stat)" != 'S' ]]; do
+      sleep 1
+    done
   done
 }
 
@@ -30,22 +32,22 @@ wait_for_docuum
 
 # This image uses ~5.5 MB.
 echo "Using an image we don't want to delete…"
-docker run alpine@sha256:52a197664c8ed0b4be6d3b8372f1d21f3204822ba432583644c9ce07f7d6448f \
+docker run --rm alpine@sha256:52a197664c8ed0b4be6d3b8372f1d21f3204822ba432583644c9ce07f7d6448f \
   true
 docker tag alpine@sha256:52a197664c8ed0b4be6d3b8372f1d21f3204822ba432583644c9ce07f7d6448f \
   alpine:keep
 
-# This image also uses ~5.5 MB.
-echo 'Using another image…'
-docker run alpine@sha256:4716d67546215299bf023fd80cc9d7e67f4bdc006a360727fd0b0b44512c45db true
+wait_for_docuum
 
 # This image also uses ~5.5 MB.
 echo 'Using another image…'
-docker run alpine@sha256:fef20cf0221c5c0eaae2d8f59081b07cd351a94ac83cdc74109b17ec90ce0a82 true
+docker run --rm alpine@sha256:4716d67546215299bf023fd80cc9d7e67f4bdc006a360727fd0b0b44512c45db true
 
-# This image also uses ~5.5 MB. Now we should be above the 20 MB threshold.
+wait_for_docuum
+
+# This image also uses ~5.5 MB. Now we should be above the 14 MB threshold.
 echo 'Using another image…'
-docker run alpine@sha256:6b987122c635cd4bf46e52d85bca765732c7a224866501742c549ccc852f8c53 true
+docker run --rm alpine@sha256:fef20cf0221c5c0eaae2d8f59081b07cd351a94ac83cdc74109b17ec90ce0a82 true
 
 wait_for_docuum
 
@@ -54,11 +56,9 @@ echo 'Checking that the protected image is still present…'
 docker inspect alpine@sha256:52a197664c8ed0b4be6d3b8372f1d21f3204822ba432583644c9ce07f7d6448f > \
   /dev/null 2>&1
 
-# Assert that the last two images are still present.
-echo 'Checking that the last two images are still present…'
+# Assert that the last image is still present.
+echo 'Checking that the last image is still present…'
 docker inspect alpine@sha256:fef20cf0221c5c0eaae2d8f59081b07cd351a94ac83cdc74109b17ec90ce0a82 > \
-  /dev/null 2>&1
-docker inspect alpine@sha256:6b987122c635cd4bf46e52d85bca765732c7a224866501742c549ccc852f8c53 > \
   /dev/null 2>&1
 
 # Assert that the first non-protected image was deleted.
