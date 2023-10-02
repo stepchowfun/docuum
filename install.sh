294d2bf02d892cc299324a074db03092e4d3e27f
Stephan Boyer <stephan@stephanboyer.com>
2023-10-02 07:44:39 -0700

    Install v0.23.1 by default

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@
   DESTINATION="${PREFIX:-/usr/local/bin}/docuum"
 
   # Which version to download
-  RELEASE="v${VERSION:-0.23.0}"
+  RELEASE="v${VERSION:-0.23.1}"
 
   # Determine which binary to download.
   FILENAME=''
